3df847c86126d3f1d235b572a6de5ef4d3afbf88
Len Wright <lenwright@lens-imac.home>
2023-04-12 12:59:50 +0100

    Added HeaderComponents  and routes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/profile" element={<Profile/>} />
-          <Route path="/signin" element={<SignIn/>} />
-          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/sign-in" element={<SignIn/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
           <Route path="/offers" element={<Offers/>} />
           <Route path="/forgotpassword" element={<ForgotPassword/>} />
         </Routes>
